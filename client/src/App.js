7917edb307175743604066f2d8b62af8b4c31c70
ced17i026 <ced17i026@iiitdm.ac.in>
2019-03-17 22:32:51 +0530

    add navbar buit with material ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Navbar from "./components/navbar";
 class App extends Component {
   render() {
     return (
       <div className="App">
-        Hello World
+        <Navbar/>
       </div>
     );
   }
